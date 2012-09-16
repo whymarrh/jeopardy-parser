0fd991c475005343c9bf040bcf6fb6440f38904d
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2012-09-16 19:06:38 -0230

    Verbose output.

--- download.sh
+++ download.sh
@@ -5,5 +5,5 @@ folder=j-archive
 mkdir -pv $folder; cd $folder
 for i in {1..3970}
 do
-  curl -O "http://j-archive.com/showgame.php?game_id=$i"
+  curl -vO "http://j-archive.com/showgame.php?game_id=$i"
 done
