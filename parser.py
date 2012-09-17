a71de9372b5cdb035cd193efce5bd45014cc3db0
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2012-09-17 02:37:26 -0230

    Rather exhaustive exception handling.

--- parser.py
+++ parser.py
@@ -4,7 +4,7 @@ import re
 from bs4 import BeautifulSoup
 
 GAME_FILES_DIR  = "j-archive/"
-NUMBER_OF_GAMES = 4
+NUMBER_OF_GAMES = 44
 
 def inclusive_range(start, stop, step = 1):
   """ A range() clone that includes the rightmost extreme value. """
@@ -44,46 +44,60 @@ def parse_game(filehandle):
   # stores the list of questions, each of which is a list itself
   questions = []
   
-  jeopardy_round = bsoup.find(id = "jeopardy_round")
-  # the list of categories for the Jeopardy round of questions
-  categories = []
-  for c in jeopardy_round.find_all("td", class_ = "category_name"):
-    categories.append(c.string.decode("string-escape"))
-  # the x-coord is which category the question fall into
-  x_coord = 0
-  for a in jeopardy_round.find_all("td", class_ = "clue"):
-    try:
-      questions.append(parse_clue(a, "1", categories[x_coord]))
-    except (AttributeError, UnicodeEncodeError):
-      continue
-    finally:
-      x_coord = 0 if x_coord == 5 else x_coord + 1
+  try:
+    jeopardy_round = bsoup.find(id = "jeopardy_round")
+    # the list of categories for the Jeopardy round of questions
+    categories = []
+    for c in jeopardy_round.find_all("td", class_ = "category_name"):
+      categories.append(c.string.decode("string-escape"))
+    # the x-coord is which category the question fall into
+    x_coord = 0
+    for a in jeopardy_round.find_all("td", class_ = "clue"):
+      try:
+        questions.append(parse_clue(a, "1", categories[x_coord]))
+      except (AttributeError, UnicodeEncodeError):
+        continue
+      finally:
+        x_coord = 0 if x_coord == 5 else x_coord + 1
+  except (AttributeError, UnicodeEncodeError):
+    # there was an error in processing
+    # the categories. ignore the round
+    pass
   
-  double_jeopardy = bsoup.find(id = "double_jeopardy_round")
-  # the list of categories for the double Jeopardy round of questions
-  categories = []
-  for c in double_jeopardy.find_all("td", class_ = "category_name"):
-    categories.append(c.string.decode("string-escape"))
-  # the x-coord is which category the question falls into
-  x_coord = 0
-  for a in double_jeopardy.find_all("td", class_ = "clue"):
-    try:
-      questions.append(parse_clue(a, "2", categories[x_coord]))
-    except (AttributeError, UnicodeEncodeError):
-      continue
-    finally:
-      x_coord = 0 if x_coord == 5 else x_coord + 1
+  try:
+    double_jeopardy = bsoup.find(id = "double_jeopardy_round")
+    # the list of categories for the double Jeopardy round of questions
+    categories = []
+    for c in double_jeopardy.find_all("td", class_ = "category_name"):
+      categories.append(c.string.decode("string-escape"))
+    # the x-coord is which category the question falls into
+    x_coord = 0
+    for a in double_jeopardy.find_all("td", class_ = "clue"):
+      try:
+        questions.append(parse_clue(a, "2", categories[x_coord]))
+      except (AttributeError, UnicodeEncodeError):
+        continue
+      finally:
+        x_coord = 0 if x_coord == 5 else x_coord + 1
+  except (AttributeError, UnicodeEncodeError):
+    # there was an error in processing
+    # the categories. ignore the round
+    pass
   
   # final Jeopardy round
-  fj = bsoup.find("table", class_ = "final_round")
-  question = ["3"]
-  question.append(fj.find("td", class_ = "category_name").get_text().decode("string-escape"))
-  question.append("nil")
-  question.append(fj.find("td", class_ = "clue_text").get_text().decode("string-escape"))
-  answer = BeautifulSoup(fj.find("div", onmouseover = True).get("onmouseover"), "lxml")
-  answer = answer.find("em").get_text().decode("string-escape")
-  question.append(answer)
-  questions.append(question)
+  try:
+    fj = bsoup.find("table", class_ = "final_round")
+    question = ["3"]
+    question.append(fj.find("td", class_ = "category_name").get_text().decode("string-escape"))
+    question.append("nil")
+    question.append(fj.find("td", class_ = "clue_text").get_text().decode("string-escape"))
+    answer = BeautifulSoup(fj.find("div", onmouseover = True).get("onmouseover"), "lxml")
+    answer = answer.find("em").get_text().decode("string-escape")
+    question.append(answer)
+    questions.append(question)
+  except:
+    # if anything goes wrong just ignore this whole clue
+    pass
   
   return [metadata, questions]
 
