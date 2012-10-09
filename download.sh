e83595fb90f8e585879c25c39ec0c7e4bf870567
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2012-10-09 13:33:33 -0230

    Upped the wait between downloads to 5s.

--- download.sh
+++ download.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 folder=j-archive
-
 mkdir $folder; cd $folder
 for i in {1..3970}
 do
   curl -vO "http://j-archive.com/showgame.php?game_id=$i"
-  sleep 2s # remember to be kind to the server
+  sleep 5s # remember to be kind to the server
 done
