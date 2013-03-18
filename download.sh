8bc1c71260556131c376d156b763e5fff865d819
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2013-03-17 22:42:33 -0230

    Spaces to tabs.

--- download.sh
+++ download.sh
@@ -4,6 +4,6 @@ folder=j-archive
 mkdir $folder; cd $folder
 for i in {1..3970}
 do
-  curl -vO "http://j-archive.com/showgame.php?game_id=$i"
-  sleep 5s # remember to be kind to the server
+	curl -vO "http://j-archive.com/showgame.php?game_id=$i"
+	sleep 5s # remember to be kind to the server
 done
