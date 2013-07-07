3904446a26681649fcc3bcd43ba3dda9a8c82071
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2013-07-07 17:54:26 -0400

    Better build script.
    The build script now takes into consideration what has already been
    downloaded, and if the file is already executable.

--- build.sh
+++ build.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
 folder="j-archive"
-mkdir "$folder"; cd "$folder"
+if [[ ! -d $folder ]]
+then
+	echo "Making $folder"
+	mkdir "$folder"; cd "$folder"
 
-for i in {1..3970}
-do
-	curl -vO "http://j-archive.com/showgame.php?game_id=$i"
-	sleep 5s # remember to be kind to the server
-done
+	echo "Downloading game files"
+	for i in {1..3970}
+	do
+		curl -vO "http://j-archive.com/showgame.php?game_id=$i"
+		sleep 5s # remember to be kind to the server
+	done
+fi
 
 script="parser.py"
-chmod +x "$script"
+if [[ ! -x $script ]]
+then
+	echo "Setting script as executable"
+	chmod +x "$script"
+fi
 
-(time "./$script") 2>&1
+echo "Running script"
+duration=`(time ./$script) 2>&1 | grep real | cut -f2`
+echo "Took $duration"
