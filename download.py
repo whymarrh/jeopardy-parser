118b5bc0f7373b02868682f5ccbc843fbee39a20
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2013-10-10 19:33:40 -0230

    Added cross-platform download script.
    I always seem to forget about Windows®.

--- download.py
+++ download.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python -OO
+# -*- coding: utf-8 -*-
+
 import itertools
 import os
 import urllib2
@@ -5,18 +8,18 @@ import time
 
 current_working_directory = os.path.dirname(os.path.abspath(__file__))
 archive_folder = os.path.join(current_working_directory, "j-archive")
-SECONDS_BETWEEN_REQUESTS = 3
+SECONDS_BETWEEN_REQUESTS = 5
 ERROR_MSG = "ERROR: No game"
+
 def main():
 	create_archive_dir()
+	print "Downloading game files"
 	download_pages()
-
 
 def create_archive_dir():
 	if not os.path.isdir(archive_folder):
-		print "Making folder %s" % archive_folder
+		print "Making %s" % archive_folder
 		os.mkdir(archive_folder)
-
 
 def download_pages():
 	for page in itertools.count(1):
@@ -25,18 +28,13 @@ def download_pages():
 		if not os.path.exists(destination_file_path):
 			html = download_page(page)
 			if ERROR_MSG in html:
-				print "Finished downloading!  Now parse!"
-				break		
+				print "Finished downloading. Now parse."
+				break
 			if html:
 				save_file(html, destination_file_path)
-				time.sleep(SECONDS_BETWEEN_REQUESTS)
-			
-			#put in some break condition based on the
-			#url getting a response other than 200
-			#so we don't hard-code the upper-limit of pages
+				time.sleep(SECONDS_BETWEEN_REQUESTS) # Remember to be kind to the server
 		else:
 			print "Already downloaded %s" % destination_file_path
-		
 
 def download_page(page):
 	url = 'http://j-archive.com/showgame.php?game_id=%s' % page
@@ -44,24 +42,20 @@ def download_page(page):
 	try:
 		response = urllib2.urlopen(url)
 		if response.code == 200:
-			print "got %s" % url
+			print "Downloading %s" % url
 			html = response.read()
 		else:
-			print "%s wasn't a good url!"
-
+			print "Invalid URL: %s" % url
 	except urllib2.HTTPError:
 		print "failed to open %s" % url
 	return html
 
-				
 def save_file(html, filename):
-	print "saving %s" % filename
 	try:
 		with open(filename, 'w') as f:
 			f.write(html)
 	except IOError:
 		print "Couldn't write to file %s" % filename
-
 
 if __name__ == "__main__":
 	main()
